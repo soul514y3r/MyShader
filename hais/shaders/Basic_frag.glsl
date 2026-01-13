4414d3e8743bd12bbbdffa34cc278c8d767e26ea
soul514y3r <120097457+soul514y3r@users.noreply.github.com>
2026-01-13 19:44:57 +0100

    Update Basic_frag.glsl BRDF

--- hais/shaders/Basic_frag.glsl
+++ hais/shaders/Basic_frag.glsl
@@ -26,6 +26,47 @@ mat3 tbnNormalTangent(vec3 normal, vec3 tangent) {
     return mat3(tangent, bitangent, normal);
 }
 
+vec3 brdf(vec3 lightDir, vec3 viewDir, float roughness, vec3 normal, vec3 albedo, float metallic, vec3 reflectance) {
+    
+    float alpha = pow(roughness,2);
+
+    vec3 H = normalize(lightDir + viewDir);
+    
+
+    //dot products
+    float NdotV = clamp(dot(normal, viewDir), 0.001,1.0);
+    float NdotL = clamp(dot(normal, lightDir), 0.001,1.0);
+    float NdotH = clamp(dot(normal,H), 0.001,1.0);
+    float VdotH = clamp(dot(viewDir, H), 0.001,1.0);
+
+    // Fresnel
+    vec3 F0 = reflectance;
+    vec3 fresnelReflectance = F0 + (1.0 - F0) * pow(1.0 - VdotH, 5.0); //Schlick's Approximation
+
+    //phong diffuse
+    vec3 rhoD = albedo;
+    rhoD *= (vec3(1.0)- fresnelReflectance); //energy conservation - light that doesn't reflect adds to diffuse
+
+    //rhoD *= (1-metallic); //diffuse is 0 for metals
+
+    // Geometric attenuation
+    float k = alpha/2;
+    float geometry = (NdotL / (NdotL*(1-k)+k)) * (NdotV / ((NdotV*(1-k)+k)));
+
+    // Distribution of Microfacets
+    float lowerTerm = pow(NdotH,2) * (pow(alpha,2) - 1.0) + 1.0;
+    float normalDistributionFunctionGGX = pow(alpha,2) / (3.14159 * pow(lowerTerm,2));
+
+    vec3 phongDiffuse = rhoD; //
+    vec3 cookTorrance = (fresnelReflectance*normalDistributionFunctionGGX*geometry)/(4*NdotL*NdotV);
+    
+    vec3 BRDF = (phongDiffuse+cookTorrance)*NdotL;
+   
+    vec3 diffFunction = BRDF;
+    
+    return BRDF;
+}
+
 void main() {
 
     
@@ -55,6 +96,17 @@ void main() {
     vec4 specularData = texture(specular, texCoord);
 
     float perceptualSmoothness = specularData.r;
+
+    float metallic = 0.0;
+
+    vec3 reflectance = vec3(0);
+
+    if(specularData.g*255 > 229) {
+        metallic = 1.0;
+        reflectance = outputColor;
+    } else{
+        reflectance = vec3(specularData.g);
+    }
     
     float roughness = pow(1.0 - perceptualSmoothness, 2.0);
 
@@ -70,13 +122,13 @@ void main() {
 
     float diffuseLight = roughness* clamp(dot(shadowLightDirection,normalWorldSpace),0.0,1.0);
 
-    float shininess = (1+(smoothness)*100);
+    float shininess = (1+(smoothness)*200);
 
     float specularLight = clamp(smoothness*pow(dot(reflectionDirection,viewDir),shininess),0,1);
 
     float ambientLight = .2;
 
-    float lightBrightness = ambientLight + diffuseLight + specularLight;
+    vec3 lightBrightness = ambientLight + brdf(shadowLightDirection, viewDir, roughness, normalWorldSpace, outputColor, metallic, reflectance);
 
     vec3 lightColor = pow(texture(lightmap, lightmapCoords).rgb,vec3(2.2));
 
